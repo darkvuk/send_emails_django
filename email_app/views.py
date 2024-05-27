a43d661c2f029a2b8931e1cd7810ce2bb6ea8df6
darkvuk <dark.vukasovic@gmail.com>
2024-05-27 09:34:33 +0200

    Added email sending feature

--- email_app/views.py
+++ email_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your views here.
+
+def index(request):
+    title = 'Naslov'
+    receivers = ['darko.vukasovic5@gmail.com']
+    message = '<h1 color="red">Tekst mejla</h1>'
+
+    send_mail(
+        subject=title,
+        message='',
+        html_message=message,
+        from_email='settings.EMAIL_HOST_USER',
+        recipient_list=receivers,
+        fail_silently=False
+    )
+
+    return HttpResponse('Done.')
--- project/settings.py
+++ project/settings.py
@@ -25,6 +25,13 @@ SECRET_KEY = 'django-insecure-b^einiltaeoz6i=#(p_4!ma^85ajjv9wcv*7+uuzn6_eg+)*ij
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'tenderi.weareai@gmail.com'
+EMAIL_HOST_PASSWORD = 'qlxavazzurjddkrg'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAILS_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 ALLOWED_HOSTS = []
 
 
